ac5585ba4b182cf1f27f5aa5521e199d77836911
hoonsdev <mysoul98@gmail.com>
2024-03-06 23:33:49 +0900

    feat: 트렌드, 추천 컴포넌트 구성

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -1,3 +1,30 @@
+'use client';
+
+import style from './followRecommend.module.css';
+
 export default function FollowRecommend() {
-  return <div>FollowRecommend</div>;
+  const user = {
+    image: '/yRsRRjGO.jpg',
+    id: 'elonmusk',
+    nickname: 'Elon Musk',
+  };
+
+  const onFollow = () => {};
+
+  return (
+    <div className={style.container}>
+      <div className={style.userLogoSection}>
+        <div className={style.userLogo}>
+          <img src={user.image} alt={user.id} />
+        </div>
+      </div>
+      <div className={style.userInfo}>
+        <div className={style.title}>{user.nickname}</div>
+        <div className={style.count}>@{user.id}</div>
+      </div>
+      <div className={style.followButton}>
+        <button onClick={onFollow}>팔로우</button>
+      </div>
+    </div>
+  );
 }
